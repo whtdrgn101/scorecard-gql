0e53d196f31bbf7ba478952db1c58eede44c3dde
Timothy A. DeWees <timothy.dewees@gmail.com>
2024-02-24 12:54:41 -0500

    Added query for list of bow types;  Rounds next;

--- src/main/java/com/tdtech/scorecardapi/controller/BowQuery.java
+++ src/main/java/com/tdtech/scorecardapi/controller/BowQuery.java
@@ -1,7 +1,9 @@
 package com.tdtech.scorecardapi.controller;
 
 import com.tdtech.scorecardapi.entity.BowResponse;
+import com.tdtech.scorecardapi.entity.BowTypeResponse;
 import com.tdtech.scorecardapi.service.BowService;
+import com.tdtech.scorecardapi.service.BowTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -15,6 +17,8 @@ public class BowQuery {
 
     @Autowired
     private BowService bowService = null;
+    @Autowired
+    private BowTypeService bowTypeService = null;
 
     @QueryMapping
     public List<BowResponse> bowList(@Argument Integer user_id) {
@@ -24,5 +28,10 @@ public class BowQuery {
     @QueryMapping
     public BowResponse bowById(@Argument Integer bow_id) {
         return bowService.getBowById((long)bow_id);
+    }
+
+    @QueryMapping
+    public List<BowTypeResponse> bowTypeList() {
+        return bowTypeService.getBowTypeList();
     }
 }
